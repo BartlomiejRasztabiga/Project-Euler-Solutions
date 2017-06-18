8e9727c659c871d3265c3430e189535d8ee000fa
BartlomiejRasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2017-06-18 11:35:59 +0200

    Problem 3

--- src/pl/rasztabiga/Problem03.java
+++ src/pl/rasztabiga/Problem03.java
@@ -1,4 +1,27 @@
 package pl.rasztabiga;
 
 public class Problem03 {
+
+    public static void main(String[] args) {
+        long numberToCheck = 600851475143L;
+
+        for(long i = 2; i <= numberToCheck / 2; i++) {
+            if (numberToCheck % i == 0) {
+                long factor = numberToCheck / i;
+                if (isPrime(factor)) {
+                    System.out.println(factor);
+                    break;
+                }
+            }
+        }
+
+    }
+
+    private static boolean isPrime(long number) {
+        for(long i = number - 1; i > 1; i--) {
+            if (number % i == 0) return false;
+        }
+
+        return true;
+    }
 }
