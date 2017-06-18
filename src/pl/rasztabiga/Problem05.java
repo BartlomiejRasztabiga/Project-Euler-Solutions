578e677be8fdd0f25e92f6fab0552d14e4b5f4cc
BartlomiejRasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2017-06-18 11:39:30 +0200

    Problem 5

---
+++ src/pl/rasztabiga/Problem05.java
@@ -0,0 +1,23 @@
+package pl.rasztabiga;
+
+public class Problem05 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (true) {
+            if(isDivisible(i)) {
+                System.out.println(i);
+                break;
+            }
+            i++;
+        }
+    }
+
+    private static boolean isDivisible(int number) {
+        for(int i = 1; i <= 20; i++) {
+            if (number % i != 0) return false;
+        }
+
+        return true;
+    }
+}
